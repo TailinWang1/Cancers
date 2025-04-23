c334d902d200166c66846e122e1d75fd34ec72f0
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:03:26 +0800

    Update Hepatoblastoma的代码.R

--- Hepatoblastoma的代码.R
+++ Hepatoblastoma的代码.R
@@ -10,7 +10,7 @@ library(dplyr)
 library(ggrepel)
 library(WGCNA)
 #DGE analysis
-Hepatoblastoma_top_table<- read.delim("GSE131329.top.table.tsv", header = TRUE, sep = "\t")
+Hepatoblastoma_top_table<- read.delim("GSE.top.table.tsv", header = TRUE, sep = "\t")
 Hepatoblastoma_top_table$diffexpressed <- "NS"
 Hepatoblastoma_top_table$diffexpressed[Hepatoblastoma_top_table$adj.P.Val< 0.05 & Hepatoblastoma_top_table$logFC > 1] <- "Up"
 Hepatoblastoma_top_table$diffexpressed[Hepatoblastoma_top_table$adj.P.Val< 0.05 & Hepatoblastoma_top_table$logFC< -1] <- "Down"
@@ -57,7 +57,7 @@ write.csv(DEG17,
           row.names = FALSE,
           quote = FALSE)
 #WGCNA
-T1D_gset <- getGEO('GSE131329', destdir=".",AnnotGPL = T,getGPL = T)
+T1D_gset <- getGEO('GSE', destdir=".",AnnotGPL = T,getGPL = T)
 T1D_exp<-exprs(T1D_gset[[1]])
 T1D_GPL<-fData(T1D_gset[[1]])
 T1D_gpl<- T1D_GPL[, c(1, 3)]
