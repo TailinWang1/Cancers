22619969e821c421cd5ea913628099cd1b5382ba
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:00:50 +0800

    Update Cervical cancer的代码.R

--- Cervical cancer的代码.R
+++ Cervical cancer的代码.R
@@ -10,7 +10,7 @@ library(dplyr)
 library(ggrepel)
 library(WGCNA)
 #DGE analysis
-Cervical_cancer_top_table<- read.delim("GSE46857.top.table.tsv", header = TRUE, sep = "\t")
+Cervical_cancer_top_table<- read.delim("GSE.top.table.tsv", header = TRUE, sep = "\t")
 Cervical_cancer_top_table$diffexpressed <- "NS"
 Cervical_cancer_top_table$diffexpressed[Cervical_cancer_top_table$adj.P.Val< 0.05 & Cervical_cancer_top_table$logFC > 1] <- "Up"
 Cervical_cancer_top_table$diffexpressed[Cervical_cancer_top_table$adj.P.Val< 0.05 & Cervical_cancer_top_table$logFC< -1] <- "Down"
@@ -56,7 +56,7 @@ write.csv(DEG05,
           row.names = FALSE,
           quote = FALSE)
 #WGCNA
-T1D_gset <- getGEO('GSE46857', destdir=".",AnnotGPL = T,getGPL = T)
+T1D_gset <- getGEO('GSE', destdir=".",AnnotGPL = T,getGPL = T)
 T1D_exp<-exprs(T1D_gset[[1]])
 T1D_GPL<-fData(T1D_gset[[1]])
 T1D_gpl<- T1D_GPL[, c(1, 2)]
