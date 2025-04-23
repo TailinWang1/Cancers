cfe02882183f0be3e831124f7b5ca6f0be7505f1
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:02:07 +0800

    Update Esophageal cancer的代码.R

--- Esophageal cancer的代码.R
+++ Esophageal cancer的代码.R
@@ -10,7 +10,7 @@ library(dplyr)
 library(ggrepel)
 library(WGCNA)
 #DGE analysis
-Esophageal_cancer_top_table<- read.delim("GSE9982.top.table.tsv", header = TRUE, sep = "\t")
+Esophageal_cancer_top_table<- read.delim("GSE.top.table.tsv", header = TRUE, sep = "\t")
 Esophageal_cancer_top_table$diffexpressed <- "NS"
 Esophageal_cancer_top_table$diffexpressed[Esophageal_cancer_top_table$adj.P.Val< 0.05 & Esophageal_cancer_top_table$logFC > 1] <- "Up"
 Esophageal_cancer_top_table$diffexpressed[Esophageal_cancer_top_table$adj.P.Val< 0.05 & Esophageal_cancer_top_table$logFC< -1] <- "Down"
@@ -54,7 +54,7 @@ write.csv(DEG23,
           row.names = FALSE,
           quote = FALSE)
 #WGCNA
-T1D_gset <- getGEO('GSE9982', destdir=".",AnnotGPL = T,getGPL = T)
+T1D_gset <- getGEO('GSE', destdir=".",AnnotGPL = T,getGPL = T)
 T1D_exp<-exprs(T1D_gset[[1]])
 T1D_GPL<-fData(T1D_gset[[1]])
 T1D_gpl<- T1D_GPL[, c(1, 3)]
