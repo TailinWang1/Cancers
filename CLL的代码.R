48630de55c2cb137c9f841e682cc6c528749fcd8
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:00:02 +0800

    Update CLL的代码.R

--- CLL的代码.R
+++ CLL的代码.R
@@ -10,7 +10,7 @@ library(dplyr)
 library(ggrepel)
 library(WGCNA)
 #DGE analysis
-CLL_top_table<- read.delim("GSE8835.top.table.tsv", header = TRUE, sep = "\t")
+CLL_top_table<- read.delim("GSE.top.table.tsv", header = TRUE, sep = "\t")
 CLL_top_table$diffexpressed <- "NS"
 CLL_top_table$diffexpressed[CLL_top_table$adj.P.Val< 0.05 & CLL_top_table$logFC > 1] <- "Up"
 CLL_top_table$diffexpressed[CLL_top_table$adj.P.Val< 0.05 & CLL_top_table$logFC< -1] <- "Down"
@@ -55,7 +55,7 @@ write.csv(DEG06,
           row.names = FALSE,
           quote = FALSE)
 #WGCNA
-T1D_gset <- getGEO('GSE8835', destdir=".",AnnotGPL = T,getGPL = T)
+T1D_gset <- getGEO('GSE', destdir=".",AnnotGPL = T,getGPL = T)
 T1D_exp<-exprs(T1D_gset[[1]])
 T1D_GPL<-fData(T1D_gset[[1]])
 T1D_gpl<- T1D_GPL[, c(1, 3)]
