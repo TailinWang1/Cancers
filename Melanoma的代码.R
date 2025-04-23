1adb9080ad620d60e4f28c5a76121993e616bd28
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:04:57 +0800

    Update Melanoma的代码.R

--- Melanoma的代码.R
+++ Melanoma的代码.R
@@ -10,7 +10,7 @@ library(dplyr)
 library(ggrepel)
 library(WGCNA)
 #DGE analysis
-Melanoma_top_table<- read.delim("GSE15605.top.table.tsv", header = TRUE, sep = "\t")
+Melanoma_top_table<- read.delim("GSE.top.table.tsv", header = TRUE, sep = "\t")
 Melanoma_top_table$diffexpressed <- "NS"
 Melanoma_top_table$diffexpressed[Melanoma_top_table$adj.P.Val< 0.05 & Melanoma_top_table$logFC > 1] <- "Up"
 Melanoma_top_table$diffexpressed[Melanoma_top_table$adj.P.Val< 0.05 & Melanoma_top_table$logFC< -1] <- "Down"
@@ -54,7 +54,7 @@ write.csv(DEG19,
           row.names = FALSE,
           quote = FALSE)
 #WGCNA
-T1D_gset <- getGEO('GSE15605', destdir=".",AnnotGPL = T,getGPL = T)
+T1D_gset <- getGEO('GSE', destdir=".",AnnotGPL = T,getGPL = T)
 T1D_exp<-exprs(T1D_gset[[1]])
 T1D_GPL<-fData(T1D_gset[[1]])
 T1D_gpl<- T1D_GPL[, c(1, 3)]
