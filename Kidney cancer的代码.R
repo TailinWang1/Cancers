9f051767d94fa63619db3abfebdf0342e365e4e3
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:03:40 +0800

    Update Kidney cancer的代码.R

--- Kidney cancer的代码.R
+++ Kidney cancer的代码.R
@@ -10,7 +10,7 @@ library(dplyr)
 library(ggrepel)
 library(WGCNA)
 #DGE analysis
-Kidney_cancer_top_table<- read.delim("GSE15641.top.table.tsv", header = TRUE, sep = "\t")
+Kidney_cancer_top_table<- read.delim("GSE.top.table.tsv", header = TRUE, sep = "\t")
 Kidney_cancer_top_table$diffexpressed <- "NS"
 Kidney_cancer_top_table$diffexpressed[Kidney_cancer_top_table$adj.P.Val< 0.05 & Kidney_cancer_top_table$logFC > 1] <- "Up"
 Kidney_cancer_top_table$diffexpressed[Kidney_cancer_top_table$adj.P.Val< 0.05 & Kidney_cancer_top_table$logFC< -1] <- "Down"
@@ -56,7 +56,7 @@ write.csv(DEG15,
           row.names = FALSE,
           quote = FALSE)
 #WGCNA
-T1D_gset <- getGEO('GSE15641', destdir=".",AnnotGPL = T,getGPL = T)
+T1D_gset <- getGEO('GSE', destdir=".",AnnotGPL = T,getGPL = T)
 T1D_exp<-exprs(T1D_gset[[1]])
 T1D_GPL<-fData(T1D_gset[[1]])
 T1D_gpl<- T1D_GPL[, c(1, 3)]
